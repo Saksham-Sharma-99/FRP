cc5cd0da202f9d7051484d95e617390f8cbe8548
Saksham-Sharma-99 <sakshamsharma99@outlook.com>
2020-12-28 15:08:10 +0530

    bug fixes

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -6,18 +6,19 @@ import { Constants, BaseURL } from "../../Model/Constants";
 
 function LocationsTop(props){
     return(
-        <div className="row nearby-img-cont" style={{paddingLeft:"0px",paddingRight:"30px"}}>
+        <div className="row locations" style={{margin:"0",padding:"0 15px"}}>
                 {(props.array).map((el)=>{
-                    return el.id==1?(
-                        <div className="col-6">
-                    <img className="nearby-img-lg" src={el.image} />
-                    <p ><a href="" className="img-txt1 img-txt">{el.title}</a></p>
-                </div>
+                    return el.id== 1?
+                    (
+                        <div className="col-lg-6">
+                            <img className="nearby-img-lg" src={el.image} />
+                            <h4 style={{position:"relative",bottom:"40px"}}>{el.title}</h4>
+                        </div>
                     ):(
-                        <div className="col-3">
-                    <img className="nearby-img-small" src={el.image} />
-                    <p ><a href="" className="img-txt2 img-txt">{el.title}</a></p>
-                </div>
+                        <div className="col-lg-3">
+                            <img className="nearby-img-small" src={el.image} />
+                            <h4 style={{position:"relative",bottom:"40px"}}>{el.title}</h4>
+                        </div>
                     )
                 })}
             </div>
@@ -26,18 +27,19 @@ function LocationsTop(props){
 
 function LocationsMid(props){
     return(
-        <div className="row nearby-img-cont" style={{paddingBottom:"1.5rem", paddingLeft:"0px",paddingRight:"30px"}}>
+        <div className="row locations" style={{padding:"0 15px",margin:"0"}}>
                 {(props.array).map((el)=>{
-                    return el.id==6?(
-                        <div className="col-6">
-                    <img className="nearby-img-lg bg-right-img" src={el.image} style={{position:"relative", bottom:"150px"}}/>
-                    <p ><a href="" className="img-txt3 img-txt">{el.title}</a></p>
-                </div>
+                    return el.id== 6?
+                    (
+                        <div className="col-lg-6">
+                            <img className="nearby-img-lg bg-right-img" src={el.image} style={{position:"relative", bottom:"150px"}}/>
+                            <h4 style={{position:"relative",bottom:"190px"}}>{el.title}</h4>
+                        </div>
                     ):(
-                        <div className="col-3">
-                    <img className="nearby-img-small" src={el.image} />
-                    <p ><a href="" className="img-txt2 img-txt">{el.title}</a></p>
-                </div>
+                        <div className="col-lg-3">
+                            <img className="nearby-img-small" src={el.image} />
+                            <h4 style={{position:"relative",bottom:"40px"}}>{el.title}</h4>
+                        </div>
                     )
                 })}
             </div>
@@ -46,16 +48,18 @@ function LocationsMid(props){
 
 function Testimonials(props){
     return (
-        <div className="row testimonials-cont" style={{paddingLeft:"80px",paddingRight:"20px", paddingBottom:"200px"}}>
-            {(props.array).map((el)=>{
+        // <div className="col-12" style={{backgroundColor:"white",margin:"0",position:"relative",top:"695px"}}>
+            (props.array).map((el)=>{
                 return(
-                    <div className="col-3">
-                    <div className="flip-card">
+                    <div className="col college-details" style={{backgroundColor:"white",paddingBottom:"50px"}}>
+                    <div className="flip-card" style={el.id<=2 ? {float:"right"}:null}>
                         <div className="flip-card-inner">
-                            <div className="flip-card-front">
-                                <img src={BaseURL+"/images/?name="+el.image} style={{width:"300px", height:"300px", borderTopLeftRadius:"10px", borderTopRightRadius:"10px"}} />
-                                <h1 style={{paddingTop:"30px", marginBottom:"25px"}}>{el.name}</h1>
-                                <h5>{el.branch}</h5>
+                            <div className="flip-card-front" >
+                                <img src={BaseURL+"/images/?name="+el.image} 
+                                style={{width:"300px", height:"300px", borderTopLeftRadius:"10px", 
+                                borderTopRightRadius:"10px"}} />
+                                <h1 style={{paddingTop:"30px", marginBottom:"25px",fontFamily:"Georgia"}}>{el.name}</h1>
+                                <h3 style={{fontFamily:"Georgia"}}>{el.branch}</h3>
                             </div>
                             <div className="flip-card-back">
                                 <blockquote>{el.quote}</blockquote>   
@@ -64,44 +68,45 @@ function Testimonials(props){
                     </div>
                 </div>  
                 );
-            })}
-        </div>
+            })
+        // </div>
     );
     
 }
 
 
-const img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYLP8ZqKpvEWHWJ8nkTDA7E-0uAVPcsfkpjg&usqp=CAU";
+
 function ProjectDetails(){
     var collegeData=JSON.parse(sessionStorage.getItem(Constants.PROJECTS)).filter((data)=>data.postId==sessionStorage.getItem(Constants.PROJECT_ID))[0]
     return(
-        <div className="container-fluid cont-fl">
-            <div className="row college-img-row">
-                <img className="university_img" src="https://c1.wallpaperflare.com/preview/812/366/509/purdue-university-west-lafayette-indiana-arch.jpg" />
+        <div className="container-fluid cont-fl" style={{padding:"0",minHeight:"700px"}}>
+            <div className="row" style={{marginLeft:"0",marginRight:"0"}}>
+                <div className="col-12 university_img" style={{backgroundImage:`url(${collegeData.image1})`}}>
+                {/* <img className="university_img" src={collegeData.image1} /> */}
                 <h2 className="college-name" >{collegeData.data.name}</h2>
-            </div>
-            <div className="row college-detail-row">
-                <div className="col-sm-12 college-details">
-                    <h1 style={{fontWeight:"bold", fontSize:"4.2rem", paddingBottom:"2rem"}}>Locations Nearby</h1>
-                    <p>Beautiful places near campus to visit and have a great experience</p>
                 </div>
             </div>
+             <div className="row college-details">
+                <div className="col-lg-12">
+                    <h1 style={{fontWeight:"bold", fontSize:"4.2rem", paddingBottom:"1rem"}}>Locations Nearby</h1>
+                    <h3>Beautiful places near campus to visit and have a great experience</h3>
+                </div>
+                <LocationsTop  array={collegeData.locationDetails.slice(0, 3)}/>
+                <LocationsMid array={collegeData.locationDetails.slice(3, 6)}/>
+            </div>
 
-            <LocationsTop  array={collegeData.locationDetails.slice(0, 3)}/>
-            <LocationsMid array={collegeData.locationDetails.slice(3, 6)}/>
+            
 
-            <div className="row">
-                <div className="col-sm-12 college-details-2">
+            <div className="row" style={{margin:"0"}}>
+                <div className="col-lg-12 college-details" style={{backgroundColor:"white",color:"#1b262c"}}>
                     <h1 style={{fontWeight:"bold", fontSize:"4.2rem", paddingBottom:"0rem"}}>Testimonials</h1>
-                    <p>Students who have previously been a part of foreign universities via FRP</p>
+                    <h4>Students who have previously been a part of foreign universities via FRP</h4>
                 </div>
+                <Testimonials array={collegeData.testimonials}/> 
             </div>
-            <Testimonials array={collegeData.testimonials}/> 
-            <div className="row college-description">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaQMaLchdp5F9ESEgb2wc6_aSO1P2m3Yj9UQ&usqp=CAU" className="college-logo" />
-                <div className="row">
-                    
-                </div>        
+
+            <div className="row college-details-2" style={{backgroundImage:`url(${collegeData.image2})`}}>
+                        
             </div>
         </div>
     );
